0c04fc720f451cc5281a0e98351f65bbe3842001
Lunarythia <63614345+lunarythia@users.noreply.github.com>
2025-12-30 10:25:27 -0800

    Prettier

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,54 +1,54 @@
-import fs, { watch } from 'fs';
-import path from 'path';
+import fs, { watch } from "fs";
+import path from "path";
 
 // import cssnano from 'cssnano';
-import postcss from 'postcss';
-import tailwindcss from '@tailwindcss/postcss';
-import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss'
+import postcss from "postcss";
+import tailwindcss from "@tailwindcss/postcss";
+import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
 
-import autoprefixer from 'autoprefixer';
+import autoprefixer from "autoprefixer";
 
 import { HtmlBasePlugin } from "@11ty/eleventy";
 
 export default async function (eleventyConfig) {
-    eleventyConfig.addWatchTarget("./src/");
+  eleventyConfig.addWatchTarget("./src/");
 
-    eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 
-    // Compile Tailwind : https://www.humankode.com/eleventy/how-to-set-up-tailwind-4-with-eleventy-3/
-    eleventyConfig.on('eleventy.after', async () => {
-        const tailwindInput = path.resolve("./src/style.css");
-        const tailwindOutput = path.resolve("./build/style.css");
-        const cssContent = fs.readFileSync(tailwindInput);
-        const outputDir = path.dirname(tailwindOutput);
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir, { recursive: true });
-        }
-        const result = await processor.process(cssContent, {
-            from: tailwindInput,
-            to: tailwindOutput
-        });
-        fs.writeFileSync(tailwindOutput, result.css);
+  // Compile Tailwind : https://www.humankode.com/eleventy/how-to-set-up-tailwind-4-with-eleventy-3/
+  eleventyConfig.on("eleventy.after", async () => {
+    const tailwindInput = path.resolve("./src/style.css");
+    const tailwindOutput = path.resolve("./build/style.css");
+    const cssContent = fs.readFileSync(tailwindInput);
+    const outputDir = path.dirname(tailwindOutput);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+    const result = await processor.process(cssContent, {
+      from: tailwindInput,
+      to: tailwindOutput,
     });
+    fs.writeFileSync(tailwindOutput, result.css);
+  });
 
-    const processor = postcss([
-        tailwindcss(),
-        autoprefixer(),
-        purgeCSSPlugin({
-            content: ["./build/**/*.html", "./src/**/*.njk"],
-            fontFace: true,
-            keyframes: true,
-            variables: true,
-        }),
-        // cssnano({
-        //     preset: 'default',
-        // }),
-    ]);
-};
+  const processor = postcss([
+    tailwindcss(),
+    autoprefixer(),
+    purgeCSSPlugin({
+      content: ["./build/**/*.html", "./src/**/*.njk"],
+      fontFace: true,
+      keyframes: true,
+      variables: true,
+    }),
+    // cssnano({
+    //     preset: 'default',
+    // }),
+  ]);
+}
 
 export const config = {
-    dir: {
-        input: "src",
-        output: "build"
-    }
+  dir: {
+    input: "src",
+    output: "build",
+  },
 };
